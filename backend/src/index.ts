2cc8958e8619792da087484708f43ca5c46dab70
justanuragmaurya <justanuragmaurya@gmail.com>
2024-12-05 02:40:22 +0530

    basic broadcasting of msg to everyone done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,23 @@
 import { WebSocketServer } from "ws";
 import WebSocket from "ws";
 
+let userCount = 0 ;
+let allSocket:WebSocket[] = [];
 const wss = new WebSocketServer({port : 8080}) 
 
 wss.on("connection",(ws:WebSocket)=>{
+    userCount++ ;
+    console.log(userCount)
+    allSocket.push(ws);
     ws.on("message",(data)=>{
-        ws.send(data.toString());
+        allSocket.map((wsss)=>{
+            wsss.send(data.toString())
+        }
+    )})
+    
+    ws.on("close",()=>{
+        userCount--;
+        console.log(userCount)  
+        allSocket = allSocket.filter(x => x !== ws);
     })
 })
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,48 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [currentMsg , setCM] = useState<any>();
+  const [ws,setWs] = useState<WebSocket | null>(null)
+  const [msgs,setMsg] = useState<string[]>([]);
+  useEffect(()=>{
+    const ws = new WebSocket("http://localhost:8080")
+    setWs(ws)
+    ws.onmessage = (e)=>{
+      setMsg(prev=>[...prev,e.data])
+    }
+  },[])
+
+  function sendMsg(){
+    if(!ws){
+      return
+    }
+      ws.send(currentMsg);
+  }
 
   return (
-    <div className='flex flex-col items-center justify-center text-center h-screen bg-[#0e0e0e] text-white'> 
-      <h2 className='text-7xl font-bold '>Chat App</h2>
-      <h3 className='text-xl'>Frontend</h3>
+    <div className='flex flex-col items-center h-screen bg-[#0e0e0e] w-screen text-white p-10'> 
+      <div className='flex flex-col items-center'>
+        <h3 className='text-3xl font-semibold'>Temp</h3>
+        <h2 className='text-7xl font-bold m-5'><span className='bg-red-500 px-2'>Chat</span> App</h2>
+      </div>
+      <div className=' flex justify-center mt-5 w-screen'>
+        <div className='flex gap-3'>
+          <input type="text " className='px-5 py-3 border-[#4e4e4e] border-[1px] rounded-md  bg-[#0e0e0e]' onChange={(e)=>{setCM(e.target.value)}}/>
+          <button className='border-[#4e4e4e] border-[1px] rounded-md px-5' onClick={sendMsg}>Send message !</button>
+        </div>
+      </div>
+      <div>
+        <h2>Messages : </h2>
+        <div className='flex flex-col'>
+          {msgs.map((e)=>{
+            return(<ul>
+              {e}
+            </ul>)
+          })}
+        </div>
+      </div>
+     
     </div>
   )
 }
