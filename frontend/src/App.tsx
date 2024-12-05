827a090b23cbf646767f21340526ef5a27a080ac
justanuragmaurya <justanuragmaurya@gmail.com>
2024-12-05 20:01:07 +0530

    minor UI tweaks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -131,7 +131,7 @@ function App() {
           <div className="flex flex-col items-center">
             <h3 className="text-2xl md:text-3xl font-semibold text-red-500">Welcome to</h3>
             <h2 className="text-5xl md:text-7xl font-bold m-5">
-              <span className="bg-red-500 px-2">Chat</span> App
+              <span className="bg-red-500 px-2">Baat</span> cheet
             </h2>
           </div>
           
